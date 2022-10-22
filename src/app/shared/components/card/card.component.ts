593a54d0a21badfbc7e379f9aa998080b32d770b
Kevin Lieder <>
2022-10-22 22:38:14 +0200

    feat: add shared card component

---
+++ src/app/shared/components/card/card.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'shared-card',
+  templateUrl: './card.component.html',
+})
+export class CardComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}

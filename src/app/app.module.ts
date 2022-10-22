16b913c0a219016712dcaf2cf68bca4bcd39dd7a
Kevin Lieder <>
2022-10-22 22:17:54 +0200

    feat: add main layout to root component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,16 @@ import { RouterModule } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, HttpClientModule, RouterModule.forRoot(routes)],
+  imports: [
+    BrowserModule,
+    CommonModule,
+    HttpClientModule,
+    RouterModule.forRoot(routes),
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,11 +7,13 @@ export const routes: Route[] = [
     pathMatch: 'full',
   },
   {
+    title: 'Detailed View',
     path: 'products-detailed',
     loadChildren: () =>
       import('./pages/detailed/detailed.module').then((m) => m.DetailedModule),
   },
   {
+    title: 'Compact View',
     path: 'products-compact',
     loadChildren: () =>
       import('./pages/compact/compact.module').then((m) => m.ComapctModule),

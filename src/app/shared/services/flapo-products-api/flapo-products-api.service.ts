39e96a21287ecbc7f8d81dd99c67379ba610c334
Kevin Lieder <>
2022-10-22 22:37:38 +0200

    refactor: moved services to shared folder

--- src/app/shared/services/flapo-products-api/flapo-products-api.service.ts
+++ src/app/shared/services/flapo-products-api/flapo-products-api.service.ts
@@ -1,14 +1,10 @@
-import { Beer } from './../../models/beer';
-import { Article } from './../../models/article';
-import { Brand } from './../../models/brand';
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs/internal/Observable';
 import { map } from 'rxjs/internal/operators/map';
-import { flatMap } from 'rxjs/internal/operators/flatMap';
-import { mergeMap } from 'rxjs/internal/operators/mergeMap';
-import { tap } from 'rxjs/internal/operators/tap';
 import { environment } from 'src/environments/environment';
+import { Brand } from 'src/app/models/brand';
+import { Beer } from 'src/app/models/beer';
 
 @Injectable({
   providedIn: 'root',
